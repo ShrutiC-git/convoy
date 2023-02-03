569556117ecea93addc013886c3bde1976966071
Shruti Chaturvedi <shrutichaturvedi16.sc@gmail.com>
2023-02-03 21:24:53 +0530

    Integrate Uffizzi

--- uffizzi/start.sh
+++ uffizzi/start.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 cp ./uffizzi/convoy-uffizzi.json .
-mv ./uffizzi/convoy-uffizz.json convoy.json
+mv ./uffizzi/convoy-uffizzi.json convoy.json
 ./cmd migrate up
 ./cmd server --config ./uffizzi/convoy-uffizzi.json
